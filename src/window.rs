1592ace4f6c2f7860ff4dfb28f8f8004a6918567
Lance Hart <shipsimfan@hotmail.com>
2022-11-18 21:01:41 -0500

    Updated to new key

--- src/window.rs
+++ src/window.rs
@@ -1,5 +1,5 @@
 use crate::{graphics::Graphics, Viewport};
-use alexandria_common::{Input, Vector2, Viewport as CommonViewport};
+use alexandria_common::{Input, Key, MouseButton, Vector2, Viewport as CommonViewport};
 use std::{cell::RefCell, ffi::CString, ptr::null, rc::Rc};
 
 pub struct Window<I: Input> {
@@ -78,14 +78,18 @@ impl<I: Input> Window<I> {
             win32::WM_WINDOWPOSCHANGED => {
                 self.update_mouse_center = true;
             }
-            win32::WM_KEYDOWN => self.input.key_down(w_param as u8),
-            win32::WM_KEYUP => self.input.key_up(w_param as u8),
-            win32::WM_LBUTTONDOWN => self.input.mouse_down(0),
-            win32::WM_LBUTTONUP => self.input.mouse_up(0),
-            win32::WM_RBUTTONDOWN => self.input.mouse_down(1),
-            win32::WM_RBUTTONUP => self.input.mouse_up(1),
-            win32::WM_MBUTTONDOWN => self.input.mouse_down(2),
-            win32::WM_MBUTTONUP => self.input.mouse_up(2),
+            win32::WM_KEYDOWN => {
+                parse_key(w_param as u8).map(|key| self.input.key_down(key));
+            }
+            win32::WM_KEYUP => {
+                parse_key(w_param as u8).map(|key| self.input.key_up(key));
+            }
+            win32::WM_LBUTTONDOWN => self.input.mouse_down(MouseButton::Primary),
+            win32::WM_LBUTTONUP => self.input.mouse_up(MouseButton::Primary),
+            win32::WM_RBUTTONDOWN => self.input.mouse_down(MouseButton::Secondary),
+            win32::WM_RBUTTONUP => self.input.mouse_up(MouseButton::Secondary),
+            win32::WM_MBUTTONDOWN => self.input.mouse_down(MouseButton::Middle),
+            win32::WM_MBUTTONUP => self.input.mouse_up(MouseButton::Middle),
             win32::WM_MOUSEMOVE => {
                 let x = (l_param & 0xFFFF) as i16;
                 let y = (l_param.wrapping_shr(16) & 0xFFFF) as i16;
@@ -324,5 +328,114 @@ impl<I: Input> alexandria_common::Window<I> for Box<Window<I>> {
 
     fn remove_viewport(&mut self, viewport: usize) {
         self.graphics.as_mut().unwrap().remove_viewport(viewport);
+    }
+}
+
+fn parse_key(key: u8) -> Option<Key> {
+    match key {
+        x if x == Key::Backspace as u8 => Some(Key::Backspace),
+        x if x == Key::Tab as u8 => Some(Key::Tab),
+        x if x == Key::Enter as u8 => Some(Key::Enter),
+        x if x == Key::Pause as u8 => Some(Key::Pause),
+        x if x == Key::CapsLock as u8 => Some(Key::CapsLock),
+        x if x == Key::Escape as u8 => Some(Key::Escape),
+        x if x == Key::Space as u8 => Some(Key::Space),
+        x if x == Key::PageUp as u8 => Some(Key::PageUp),
+        x if x == Key::PageDown as u8 => Some(Key::PageDown),
+        x if x == Key::End as u8 => Some(Key::End),
+        x if x == Key::Home as u8 => Some(Key::Home),
+        x if x == Key::LeftArrow as u8 => Some(Key::LeftArrow),
+        x if x == Key::UpArrow as u8 => Some(Key::UpArrow),
+        x if x == Key::RightArrow as u8 => Some(Key::RightArrow),
+        x if x == Key::DownArrow as u8 => Some(Key::DownArrow),
+        x if x == Key::PrintScreen as u8 => Some(Key::PrintScreen),
+        x if x == Key::Insert as u8 => Some(Key::Insert),
+        x if x == Key::Delete as u8 => Some(Key::Delete),
+        x if x == Key::_0 as u8 => Some(Key::_0),
+        x if x == Key::_1 as u8 => Some(Key::_1),
+        x if x == Key::_2 as u8 => Some(Key::_2),
+        x if x == Key::_3 as u8 => Some(Key::_3),
+        x if x == Key::_4 as u8 => Some(Key::_4),
+        x if x == Key::_5 as u8 => Some(Key::_5),
+        x if x == Key::_6 as u8 => Some(Key::_6),
+        x if x == Key::_7 as u8 => Some(Key::_7),
+        x if x == Key::_8 as u8 => Some(Key::_8),
+        x if x == Key::_9 as u8 => Some(Key::_9),
+        x if x == Key::A as u8 => Some(Key::A),
+        x if x == Key::B as u8 => Some(Key::B),
+        x if x == Key::C as u8 => Some(Key::C),
+        x if x == Key::D as u8 => Some(Key::D),
+        x if x == Key::E as u8 => Some(Key::E),
+        x if x == Key::F as u8 => Some(Key::F),
+        x if x == Key::G as u8 => Some(Key::G),
+        x if x == Key::H as u8 => Some(Key::H),
+        x if x == Key::I as u8 => Some(Key::I),
+        x if x == Key::J as u8 => Some(Key::J),
+        x if x == Key::K as u8 => Some(Key::K),
+        x if x == Key::L as u8 => Some(Key::L),
+        x if x == Key::M as u8 => Some(Key::M),
+        x if x == Key::N as u8 => Some(Key::N),
+        x if x == Key::O as u8 => Some(Key::O),
+        x if x == Key::P as u8 => Some(Key::P),
+        x if x == Key::Q as u8 => Some(Key::Q),
+        x if x == Key::R as u8 => Some(Key::R),
+        x if x == Key::S as u8 => Some(Key::S),
+        x if x == Key::T as u8 => Some(Key::T),
+        x if x == Key::U as u8 => Some(Key::U),
+        x if x == Key::V as u8 => Some(Key::V),
+        x if x == Key::W as u8 => Some(Key::W),
+        x if x == Key::X as u8 => Some(Key::X),
+        x if x == Key::Y as u8 => Some(Key::Y),
+        x if x == Key::Z as u8 => Some(Key::Z),
+        x if x == Key::Windows as u8 => Some(Key::Windows),
+        x if x == Key::Numpad0 as u8 => Some(Key::Numpad0),
+        x if x == Key::Numpad1 as u8 => Some(Key::Numpad1),
+        x if x == Key::Numpad2 as u8 => Some(Key::Numpad2),
+        x if x == Key::Numpad3 as u8 => Some(Key::Numpad3),
+        x if x == Key::Numpad4 as u8 => Some(Key::Numpad4),
+        x if x == Key::Numpad5 as u8 => Some(Key::Numpad5),
+        x if x == Key::Numpad6 as u8 => Some(Key::Numpad6),
+        x if x == Key::Numpad7 as u8 => Some(Key::Numpad7),
+        x if x == Key::Numpad8 as u8 => Some(Key::Numpad8),
+        x if x == Key::Numpad9 as u8 => Some(Key::Numpad9),
+        x if x == Key::Multiply as u8 => Some(Key::Multiply),
+        x if x == Key::Add as u8 => Some(Key::Add),
+        x if x == Key::Seperator as u8 => Some(Key::Seperator),
+        x if x == Key::Subtract as u8 => Some(Key::Subtract),
+        x if x == Key::Decimal as u8 => Some(Key::Decimal),
+        x if x == Key::Divide as u8 => Some(Key::Divide),
+        x if x == Key::F1 as u8 => Some(Key::F1),
+        x if x == Key::F2 as u8 => Some(Key::F2),
+        x if x == Key::F3 as u8 => Some(Key::F3),
+        x if x == Key::F4 as u8 => Some(Key::F4),
+        x if x == Key::F5 as u8 => Some(Key::F5),
+        x if x == Key::F6 as u8 => Some(Key::F6),
+        x if x == Key::F7 as u8 => Some(Key::F7),
+        x if x == Key::F8 as u8 => Some(Key::F8),
+        x if x == Key::F9 as u8 => Some(Key::F9),
+        x if x == Key::F10 as u8 => Some(Key::F10),
+        x if x == Key::F11 as u8 => Some(Key::F11),
+        x if x == Key::F12 as u8 => Some(Key::F12),
+        x if x == Key::F13 as u8 => Some(Key::F13),
+        x if x == Key::F14 as u8 => Some(Key::F14),
+        x if x == Key::F15 as u8 => Some(Key::F15),
+        x if x == Key::F16 as u8 => Some(Key::F16),
+        x if x == Key::F17 as u8 => Some(Key::F17),
+        x if x == Key::F18 as u8 => Some(Key::F18),
+        x if x == Key::F19 as u8 => Some(Key::F19),
+        x if x == Key::F20 as u8 => Some(Key::F20),
+        x if x == Key::F21 as u8 => Some(Key::F21),
+        x if x == Key::F22 as u8 => Some(Key::F22),
+        x if x == Key::F23 as u8 => Some(Key::F23),
+        x if x == Key::F24 as u8 => Some(Key::F24),
+        x if x == Key::NumLock as u8 => Some(Key::NumLock),
+        x if x == Key::ScrollLock as u8 => Some(Key::ScrollLock),
+        x if x == Key::LeftShift as u8 => Some(Key::LeftShift),
+        x if x == Key::RightShift as u8 => Some(Key::RightShift),
+        x if x == Key::LeftControl as u8 => Some(Key::LeftControl),
+        x if x == Key::RightControl as u8 => Some(Key::RightControl),
+        x if x == Key::LeftAlt as u8 => Some(Key::LeftAlt),
+        x if x == Key::RightAlt as u8 => Some(Key::RightAlt),
+        _ => None,
     }
 }
